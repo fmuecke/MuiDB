039efe63cbe3341a00d6b4171b33d5b90f821ed2
Florian Mücke <fmuecke@gmail.com>
2016-08-29 08:52:52 +0200

    nuspec: emoved development dependencies

--- create_nupkg.cmd
+++ create_nupkg.cmd
@@ -1,2 +1,5 @@
+@echo off
 nuget pack MuiDB.Tool\MuiDB.Tool.csproj -Build -Symbols -Properties Configuration=Release -Tool -NoPackageAnalysis
+if errorlevel 1 pause
 nuget pack MuiDB\MuiDB.Schema.nuspec
+if errorlevel 1 pause
